cb4b40373ece0212bdd2b999daf6cc3503768f4f
Orlando J Betancourth <orlando.betancourth@gmail.com>
2019-09-23 19:04:41 -0600

    Iniciando Rutas Post y Collección en Postman

--- storebackend/routes/api/products/index.js
+++ storebackend/routes/api/products/index.js
@@ -8,4 +8,37 @@ router.get('/', function (req, res) {
   });
 }); //get
 
+/** 
+ * get      Consultas     select
+ * post     Crear         insert
+ * put      Actualizar    update
+ * delete   Eliminar      delete
+*/
+
+var prodCollection = [];
+
+router.get('/all', function(req, res){
+  res.json(prodCollection);
+}); // get /all
+
+router.post('/new', function(req, res){
+   var newProduct = Object.assign({}, req.body);
+   var productExists = prodCollection.find(
+     function(o, i){
+       return o.sku === newProduct.sku;
+     }
+   )
+   if( ! productExists ){
+     prodCollection.push(newProduct);
+     res.json(newProduct);  // req.body ===  $_POST[]
+   } else {
+     res.status(400).json({"error":"No se pudo ingresar objeto"});
+   }
+}); // post /new
+
+// endPoint ==  ejeutar operacion| obtenerRecurso| guardarrecurso 
+//              | > devuelve un recurso
+
+// router  get| post | put | delete
+
 module.exports = router;
