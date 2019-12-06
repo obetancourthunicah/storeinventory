6a36ad10de85cc9fc87c0b9ec2da9d8cd618b518
Orlando J Betancourth <orlando.betancourth@gmail.com>
2019-12-05 18:25:44 -0600

    More Detail

--- storefrontend/src/components/pages/private/productform/productform.js
+++ storefrontend/src/components/pages/private/productform/productform.js
@@ -5,10 +5,22 @@ import {useStateValue} from '../../../utilities/context';
 
 export default (props)=>{
   const [ {prods}, dispatch] = useStateValue();
+  if(!prods.currentId && true){
+      props.history.replace("/product");
+  }else{
+  const CurrenProd = prods.products.filter((o)=>{
+    return o._id === prods.currentId;
+  })[0];
   return (
     <Page>
       <h1>Mostrando Producto</h1>
-      <span>{prods.currentId}</span>
+      <span><b>SKU</b></span>
+      <span>{CurrenProd.sku}</span>
+      <span><b>Producto</b></span>
+      <span>{CurrenProd.product}</span>
+      <span><b>Inventario</b></span>
+      <span>{CurrenProd.stock}</span>
     </Page>
-  )
+  );
+  }
 }
